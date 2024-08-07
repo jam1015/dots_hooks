3cb7f8a4571cd32f1b22c0dc6fa2c46fccb050cc
Jordan Mandel <jordan.mandel@live.com>
2024-08-07 13:29:48 -0400

    configged

--- config.bash
+++ config.bash
@@ -2,8 +2,8 @@ export DOTSTRYREBASE="true"
 
 # ours vs theirs in rebase/merge is counterintuitive; this uses the intiutive names; so everything is treated like a merge
 # https://stackoverflow.com/questions/25576415/what-is-the-precise-meaning-of-ours-and-theirs-in-git
-export DOTSREBASESTRATEGY="ours"
-export DOTSMERGESTRATEGY="ours"
+export DOTSREBASESTRATEGY="theirs"
+export DOTSMERGESTRATEGY="theirs"
 export DOTSPULL=""   # variable to control pull actions
 export DOTSPUSH=""   # variable to control push actions
 export REAPPLYCHERRYPICKS="false"
