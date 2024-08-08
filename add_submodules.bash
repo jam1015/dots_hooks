8ff6e09fb937d13a94facff24c5912af9d5e974c
Jordan Mandel <jordan.mandel@live.com>
2024-08-08 13:41:57 -0400

    really added submodules script

---
+++ add_submodules.bash
@@ -0,0 +1,12 @@
+#/bin/bash
+# Initialize the .config/emacs submodule
+git submodule add git@github.com:plexus/chemacs2 .config/emacs
+
+# Initialize the .oh-my-zsh submodule
+git submodule add git@github.com:ohmyzsh/ohmyzsh.git .oh-my-zsh
+
+# Initialize the .spacemacs submodule with the 'develop' branch
+git submodule add -b develop git@github.com:syl20bnr/spacemacs .spacemacs
+
+# Initialize the .doomemacs submodule
+git submodule add git@github.com:doomemacs/doomemacs .doomemacs
