0bbdfbfdb2a15c6fe7d416cd44e33a63c3d7ec81
Jordan Mandel <jordan.mandel@live.com>
2024-08-17 14:34:08 -0400

    added basic recursion

--- config.bash
+++ config.bash
@@ -1,11 +1,15 @@
+
+
+# some are options with a specific  value, some should be empty string to be set as false
 export DOTSTRYREBASE="true"
 
 # ours vs theirs in rebase/merge is counterintuitive; this uses the intiutive names; so everything is treated like a merge
 # https://stackoverflow.com/questions/25576415/what-is-the-precise-meaning-of-ours-and-theirs-in-git
-export DOTSREBASESTRATEGY="ours"
-export DOTSMERGESTRATEGY="ours"
+export DOTSREBASESTRATEGY="theirs"
+export DOTSMERGESTRATEGY="theirs"
 export DOTSPULL=""   # variable to control pull actions
 export DOTSPUSH=""   # variable to control push actions
 export REAPPLYCHERRYPICKS=""
 export RUN="true"
 export STOW=""
+export BREADTHFIRST=""
